570243007142590bdbf7211c50539a54e298ceea
Chamuditha-Jayasanka <155737652+Chamuditha-Jayasanka@users.noreply.github.com>
2025-07-31 17:55:36 +0530

    add mvc to save data

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,8 @@ module com.example.project001 {
 
     opens com.example.project001 to javafx.fxml;
     exports com.example.project001;
+    exports com.example.project001.tm;
+    opens com.example.project001.tm to javafx.fxml;
+    exports com.example.project001.controller;
+    opens com.example.project001.controller to javafx.fxml;
 }
